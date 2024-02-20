a66fef315d309c4515144aed4bfa5f9310e66c02
Ahmed Mohamed <106612109+ArmatGame@users.noreply.github.com>
2024-02-20 22:13:44 +0200

    Delete Progress Hight Page directory

--- Progress Hight Page/assets/dist/js/script.js
+++
@@ -1,8 +0,0 @@
-let progress = document.querySelector(".progress");
-
-window.onscroll = function () {
-  progress.style.width = `${
-    (window.scrollY / (document.body.scrollHeight - window.innerHeight)) * 100
-  }%`;
-};
-
